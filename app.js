91bd1e283ee1c00d4f59e220836a0cb1fafc38de
SajinStar <69190568+SajinStar@users.noreply.github.com>
2021-08-23 23:21:42 +0530

    finish without responsive

--- app.js
+++ app.js
@@ -1,6 +1,5 @@
 var swiper = new Swiper(".mySwiper1", {
     cssMode: true,
-    loop:true,
     navigation: {
       nextEl: ".swiper-button-next",
       prevEl: ".swiper-button-prev",
@@ -11,3 +10,34 @@ var swiper = new Swiper(".mySwiper1", {
     mousewheel: true,
     keyboard: true,
   });
+
+  
+var swiper = new Swiper(".mySwiper2", {
+  cssMode: true,
+  slidesPerView: 1,
+  spaceBetween: 30,
+  loop: true,
+  pagination: {
+    el: ".swiper-pagination",
+    clickable: true,
+  },
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
+  },
+});
+  
+var swiper = new Swiper(".mySwiper3", {
+  cssMode: true,
+  slidesPerView: 3.5,
+  spaceBetween: 10,
+  loop: true,
+  pagination: {
+    el: ".swiper-pagination",
+    clickable: true,
+  },
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
+  },
+});
