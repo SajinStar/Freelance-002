bb72b6913048f9d033e76655b9b41e6454d70b35
SajinStar <69190568+SajinStar@users.noreply.github.com>
2021-08-24 20:47:39 +0530

    done

--- app.js
+++ app.js
@@ -1,18 +1,18 @@
-var swiper = new Swiper(".mySwiper1", {
-    cssMode: true,
-    navigation: {
-      nextEl: ".swiper-button-next",
-      prevEl: ".swiper-button-prev",
-    },
-    pagination: {
-      el: ".swiper-pagination",
-    },
-    mousewheel: true,
-    keyboard: true,
-  });
+var swiper1 = new Swiper(".mySwiper1", {
+  cssMode: true,
+  loop: true,
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
+  },
+  pagination: {
+    el: ".swiper-pagination",
+  },
+  mousewheel: true,
+  keyboard: true,
+});
 
-  
-var swiper = new Swiper(".mySwiper2", {
+var swiper2 = new Swiper(".mySwiper2", {
   cssMode: true,
   slidesPerView: 1,
   spaceBetween: 30,
@@ -26,18 +26,23 @@ var swiper = new Swiper(".mySwiper2", {
     prevEl: ".swiper-button-prev",
   },
 });
-  
-var swiper = new Swiper(".mySwiper3", {
-  cssMode: true,
-  slidesPerView: 3.5,
-  spaceBetween: 10,
-  loop: true,
-  pagination: {
-    el: ".swiper-pagination",
-    clickable: true,
-  },
-  navigation: {
-    nextEl: ".swiper-button-next",
-    prevEl: ".swiper-button-prev",
-  },
+
+window.addEventListener("DOMContentLoaded", () => {
+  var swiper3 = new Swiper(".mySwiper3", {
+    cssMode: true,
+    slidesPerView: 3.5,
+    spaceBetween: 10,
+    loop: true,
+    pagination: {
+      el: ".swiper-pagination",
+      clickable: true,
+    },
+    navigation: {
+      nextEl: ".swiper-button-next",
+      prevEl: ".swiper-button-prev",
+    },
+    autoplay: true,
+    disableOnInteraction: true,
+    speed: 300,
+  });
 });
